70f824e04e4077998cc232084b10a816c800354f
indrianaagustin <indryanaagustin@gmail.com>
2025-05-16 14:50:17 +0700

    finish

--- lib/main.dart
+++ lib/main.dart
@@ -1,22 +1,69 @@
 import 'package:flutter/material.dart';
-import 'todo_home_page.dart'; // Pastikan file ini ada
+import 'package:shared_preferences/shared_preferences.dart';
+import 'home_page.dart';
 
 void main() {
   runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({super.key});
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  bool isDark = false;
+
+  @override
+  void initState() {
+    super.initState();
+    _loadTheme();
+  }
+
+  void _loadTheme() async {
+    final prefs = await SharedPreferences.getInstance();
+    setState(() {
+      isDark = prefs.getBool('isDark') ?? false;
+    });
+  }
+
+  void _toggleTheme(bool value) async {
+    final prefs = await SharedPreferences.getInstance();
+    setState(() {
+      isDark = value;
+    });
+    await prefs.setBool('isDark', value);
+  }
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'Todo App',
+      title: 'ToDo List',
       theme: ThemeData(
-        primarySwatch: Colors.blue,
+        brightness: Brightness.light,
+        fontFamily: 'Roboto',
+        colorScheme: ColorScheme.fromSeed(
+          seedColor: Colors.teal,
+          brightness: Brightness.light, // DITAMBAHKAN agar cocok
+        ),
+        useMaterial3: true,
       ),
-      home: const TodoHomePage(),
+      darkTheme: ThemeData(
+        brightness: Brightness.dark,
+        fontFamily: 'Roboto',
+        colorScheme: ColorScheme.fromSeed(
+          seedColor: Colors.teal,
+          brightness: Brightness.dark, // DITAMBAHKAN agar cocok
+        ),
+        useMaterial3: true,
+      ),
+      themeMode: isDark ? ThemeMode.dark : ThemeMode.light,
+      home: HomePage(
+        isDark: isDark,
+        onThemeChanged: _toggleTheme,
+      ),
     );
   }
 }
